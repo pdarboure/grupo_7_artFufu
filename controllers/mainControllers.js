be0761660c2cfe10e9f983b6f4bb9d8044b536a9
matiaschaves01 <matiaschaves01@gmail.com>
2023-01-28 18:38:42 -0300

    partials

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -6,22 +6,22 @@ const path = require("path");
 
 const mainController = {
     home: (req, res) => {
-        res.sendFile(path.resolve('./views/home.html'));
+        res.render(path.resolve('./views/home.ejs'));
     },
     login: (req, res) => {
-        res.sendFile(path.resolve('./views/login.html'));
+        res.render(path.resolve('./views/login.ejs'));
     },
     register: (req, res) => {
-        res.sendFile(path.resolve('./views/register.html'));
+        res.render(path.resolve('./views/register.ejs'));
     },
     detalleproducto: (req, res) => {
-        res.sendFile(path.resolve('./views/detalleproducto.html'));
+        res.render(path.resolve('./views/detalleproducto.ejs'));
     },
     fibrofacil: (req, res) => {
-        res.sendFile(path.resolve('./views/fibrofacil.html'));
+        res.render(path.resolve('./views/fibrofacil.ejs'));
     },
     carritoDeCompras: (req, res) => {
-        res.sendFile(path.resolve('./views/carritoDeCompras.html'));
+        res.render(path.resolve('./views/carritoDeCompras.ejs'));
     }
 };
 
