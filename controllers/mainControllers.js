5b672ceb9c2f9cd76602cd173b5f7d5c54724f97
Ignacio <ignaciolanusse1@gmail.com>
2023-01-28 19:01:45 -0300

    arreglo controller

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -6,22 +6,6 @@ const path = require("path");
 
 const mainController = {
     home: (req, res) => {
-        res.render(path.resolve('./views/home.ejs'));
-    },
-    login: (req, res) => {
-        res.render(path.resolve('./views/login.ejs'));
-    },
-    register: (req, res) => {
-        res.render(path.resolve('./views/register.ejs'));
-    },
-    detalleproducto: (req, res) => {
-        res.render(path.resolve('./views/detalleproducto.ejs'));
-    },
-    fibrofacil: (req, res) => {
-        res.render(path.resolve('./views/fibrofacil.ejs'));
-    },
-    carritoDeCompras: (req, res) => {
-        res.render(path.resolve('./views/carritoDeCompras.ejs'));
         res.render('home');
     },
     login: (req, res) => {
