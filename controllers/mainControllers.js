1bde8f92272efac89ff6ec4d2ffdd028e2d8c9ce
matiaschaves01 <matiaschaves01@gmail.com>
2023-01-31 16:27:10 -0300

    Cambiando rutas y Controladores

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -6,31 +6,57 @@ const path = require("path");
 
 const mainController = {
     home: (req, res) => {
-        res.render('home');
+        res.render('home',{
+            css: './css/homestyles.css',
+            title: 'Artistica Fufu'
+        });
     },
     login: (req, res) => {
-        res.render('login');
+        res.render('login',{
+            css: './css/login.css',
+            title: 'Login'
+        });
     },
     register: (req, res) => {
-        res.render('register');
+        res.render('register',{
+           css: './css/register.css',
+           title: 'Registro' 
+        });
     },
     detalleproducto: (req, res) => {
-        res.render('detalleproducto');
+        res.render('detalleproducto',{
+            css: './css/detalleproducto.css',
+            title: 'Detalle Producto'
+        });
     },
     fibrofacil: (req, res) => {
-        res.render('fibrofacil');
+        res.render('fibrofacil',{
+            css: './css/fibrofacil.css',
+            title: 'Fibrofacil'
+        });
     },
     carritoDeCompras: (req, res) => {
-        res.render('carritoDeCompras');
+        res.render('carritoDeCompras',{
+            css: './css/carrito-styles.css'
+        });
     },
     admin: (req, res) => {
-        res.render('admin');
+        res.render('admin',{
+            css: './css/admin.css',
+            title: 'administrador'
+        });
     },
     crearProducto: (req, res) => {
-        res.render('crearProducto');
+        res.render('crearProducto',{
+            css:'./css/admin.css',
+            title: 'Crear Producto'
+        });
     },
     editarProducto: (req, res) => {
-        res.render('editarProducto');
+        res.render('editarProducto',{
+            css: './css/admin.css',
+            title: 'Editar Producto'
+        });
     },
 };
 
