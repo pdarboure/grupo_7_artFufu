b9fa018a8f30768a906758524b67d7ad5639fa53
Pedro Agustin Darboure <119804246+pdarboure@users.noreply.github.com>
2023-02-11 15:02:00 -0300

    se sube admni que funca create/edit

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -17,6 +17,7 @@ const productController = {
         let product = productController.getProducts().find(product => product.id == productId);
         
         res.render('products/show', {
+            
             title: 'Mi producto',
             producto: product
         });
