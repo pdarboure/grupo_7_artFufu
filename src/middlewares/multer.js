beadf72faddfc8388bff0410844d1583746eafc8
Pedro Agustin Darboure <119804246+pdarboure@users.noreply.github.com>
2023-02-11 11:09:46 -0300

    ultimas modificaciones multer

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const path = require('path');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../../public/img/products'));
+        cb(null, path.join(__dirname, '../../public/images/products'));
     },
     filename: function (req, file, cb) {
         cb(null, 'product-' + Date.now() + path.extname(file.originalname));
