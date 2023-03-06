9dbd85548435b9891694a51a63b1333c42188190
matiaschaves01 <matiaschaves01@gmail.com>
2023-03-06 18:27:56 -0300

    validaciones

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const {validationResult} = require ('express-validator');
+const User = require('../services/user');
+const bcrypt = require('bcrypt')
 
 const userController = { 
     login: (req, res) => {
@@ -16,15 +18,25 @@ const userController = {
         });
     },
     registerProcess: (req,res) => {
-    let error = validationResult (req)
-    if (!error.isEmpty()) {
-        return res.render('./user/register',{
-            css: './css/register.css',
-            title: 'Registro' ,
-            error: error.mapped(),
-            oldBody: req.body
-         });
-    }
+        console.log(req.file);
+        let error = validationResult (req)
+        let body = {
+            ...req.body,
+            passwordRegistro: bcrypt.hashSync(req.body.passwordRegistro, 10),
+            image: req.file? req.file.filename:'No carga la imagen'
+        }
+        delete body.checkPassword
+
+        if (!error.isEmpty()) {
+            return res.render('./user/register',{
+                css: './css/register.css',
+                title: 'Registro' ,
+                error: error.mapped(),
+                oldBody: req.body
+            });
+        }
+        User.createUser(body)
+        res.redirect('/')
     },
     admin: (req, res) => {
         res.render('admin',{
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userControllers');
 const userValidator = require('../middlewares/userValidator');
+const upload = require('../middlewares/multer');
 
 router.get("/admin", userController.admin);
 router.get("/login", userController.login);
 router.get("/register", userController.register);
-router.post("/register",userValidator, userController.registerProcess)
+router.post("/register",upload.single('image'),userValidator, userController.registerProcess)
 
 module.exports = router
