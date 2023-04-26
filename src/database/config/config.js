6ac3336902d4a5fc174dd615f7e5818391c54de2
Pedro Agustin Darboure <119804246+pdarboure@users.noreply.github.com>
2023-04-26 19:25:41 -0300

    cambie config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,21 +1,22 @@
-module.exports = {
+module.exports={
   "development": {
     "username": "root",
-    "password": '',
-    "database": "prueba11",
+    "password": "root",
+    "database": "art_fufu3",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port":"3006"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
