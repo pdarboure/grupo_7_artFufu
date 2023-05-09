61fa4a456cba72f18398d6c631469ca0ce368617
ojsteward <oliversteward98@gmail.com>
2023-05-09 13:15:12 -0300

    Agregar Menu hamburguesa a MAIN

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
-    "database": "Art_Fufu4",
+    "password": "",
+    "database": "fufu_db",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "port": 3006,
