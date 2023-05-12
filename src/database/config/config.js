f64b82370090ded9f08f1a2ddc86b6180ee00618
ojsteward <oliversteward98@gmail.com>
2023-05-12 16:37:16 -0300

    Log out button

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": "",
     "database": "fufu_db",
     "host": "127.0.0.1",
     "dialect": "mysql",
