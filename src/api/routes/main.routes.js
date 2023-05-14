8a6885eb472de5ce52898cda1b9cee398e4f04e4
Pedro Agustin Darboure <119804246+pdarboure@users.noreply.github.com>
2023-05-14 11:27:57 -0300

    elimino carpetas creadas

--- src/api/routes/main.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const mainController = require('../controller/main.controller');
-
-router.get('/', mainController.home);
-
-module.exports = router
