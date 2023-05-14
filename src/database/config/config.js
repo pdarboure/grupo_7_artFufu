4f5a463a52abb0503de1aae406986fabcff896d4
nacholanusse <ignaciolanusse1@gmail.com>
2023-05-14 15:59:15 -0300

    detalle

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": "root",
     "database": "fufu_db",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": 3306,
+    "port": 3006,
   },
   "test": {
     "username": "root",
