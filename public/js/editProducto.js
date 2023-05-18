2cdf7da14e2e58137b213745699ff35a9a7467eb
nacholanusse <ignaciolanusse1@gmail.com>
2023-05-17 23:42:32 -0300

    intento validaciones EditProduct

--- public/js/editProducto.js
+++ public/js/editProducto.js
@@ -15,6 +15,24 @@ let contentError = document.getElementById('error');
 function mostrarError(error) {
     contentError.innerHTML = `<p class='error'>${error}</p>`
 }
+function mostrarErrorNombre(error) {
+    contentError.innerHTML = `<p class='errorNombre'>${error}</p>`
+}
+function mostrarErrorDescription(error) {
+    contentError.innerHTML = `<p class='errorDescritcion'>${error}</p>`
+}
+function mostrarErrorCantidad(error) {
+    contentError.innerHTML = `<p class='errorCantidad'>${error}</p>`
+}
+function mostrarErrorPrice(error) {
+    contentError.innerHTML = `<p class='errorPrice'>${error}</p>`
+}
+function mostrarErrorImage(error) {
+    contentError.innerHTML = `<p class='errorPrecio'>${error}</p>`
+}
+function mostrarErrorCategory(error) {
+    contentError.innerHTML = `<p class='errorPrecio'>${error}</p>`
+}
 
 
 // validar el CADA ERROR EN EL EVENTO BLUR
@@ -27,17 +45,16 @@ function validarFormulario(evt) {
         if (!input.value.length) {
             input.style.borderBottomColor = 'red';
             input.style.color = 'red';
-            // input.style.backgroundColor = "rgb(168, 110, 110)"; 
+
             if (atributte == "name") {
                 campo = "nombre"
             }
             if (atributte == "description") {
-                campo = "descripcion"
-            }
-            if (atributte == "description") {
                 campo = "description"
             }
-
+            if (atributte == "cantidad") {
+                campo = "cantidad"
+            }
             if (atributte == "price") {
                 campo = "precio"
             }
@@ -47,35 +64,39 @@ function validarFormulario(evt) {
             if (atributte == "product_categories_id") {
                 campo = "categoria"
             }
-        }
-        mostrarError("Debe ingresar un valor en el campo " + campo + "!")
+            mostrarError("Debe ingresar un valor en el campo " + campo + "!")
 
-    } else {
-        input.style.borderBottomColor = 'green';
-        input.style.color = 'green';
+        } else {
+            input.style.borderBottomColor = 'green';
+            input.style.color = 'green';
+        }
     }
 }
 
 formulario.addEventListener("submit", (e)=>{
-    if (formulario.name.value.length <= 5) {
+    if (formulario.name.value.length < 3) {
         e.preventDefault();
-        alert("Debes ingresar un nombre de mas de cinco caracteres")
+        mostrarErrorNombre("Debes ingresar un nombre de mas de 3 caracteres")
     }
-    if (formulario.name2.value.length <= 20) {
+    if (formulario.description.value.length < 15) {
         e.preventDefault();
-        alert("Debes ingresar una descripcion de mas de veinte caracteres")
+        mostrarErrorDescription("Debes ingresar una descripcion de mas de 15 caracteres")
     }
-    if (formulario.price.value == "") {
+    if (formulario.cantidad.value.length < 15) {
         e.preventDefault();
-        alert("Debes ingresar un precio")
+        mostrarErrorCantidad("Debes ingresar una descripcion de mas de 15 caracteres")
     }
-    if (formulario.image.value == "") {
+    if (formulario.price.value < 0) {
         e.preventDefault();
-        alert("Debes ingresar una imagen")
+        mostrarErrorPrice("Debes ingresar un precio mayor a 0")
     }
-    if (formulario.product_categories_id.value == "") {
+    if (formulario.image.value === !image.value) {
         e.preventDefault();
-        alert("Debes ingresar una categoria del producto")
+        mostrarErrorImage("Debes ingresar una imagen")
+    }
+    if (formulario.product_categories_id.value === !product_categories_id.value) {
+        e.preventDefault();
+        mostrarErrorCategory("Debes ingresar una categoria del producto")
     }
 
 
